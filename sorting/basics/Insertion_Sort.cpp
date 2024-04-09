570da3c0f86e59e93068d02a30cc18d046928d0b
Vaibhav Badhani <badhanivaibhav@gmail.com>
2024-04-09 17:25:01 +0530

    Create Insertion_Sort.cpp

---
+++ sorting/basics/Insertion_Sort.cpp
@@ -0,0 +1,12 @@
+void insertionSort(int arr[], int n)
+{
+    for (int i = 0; i < n ; i++) {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+}
