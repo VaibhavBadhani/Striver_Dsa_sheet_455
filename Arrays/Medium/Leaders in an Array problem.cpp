dc04d16d506455435c4c3a177cdfcb865b9938fb
Vaibhav Badhani <badhanivaibhav@gmail.com>
2024-04-12 00:11:34 +0530

    Create Leaders in an Array problem.cpp

---
+++ Arrays/Medium/Leaders in an Array problem.cpp
@@ -0,0 +1,14 @@
+vector<int> superiorElements(vector<int>&a) {
+    int n = a.size();
+    int maxi = INT_MIN;
+    vector<int>ans;
+    for(int i =n-1;i>=0;i--){
+        if(a[i]>maxi){
+            ans.push_back(a[i]);
+        }
+        maxi = max(maxi,a[i]);
+    }
+    sort(ans.begin(),ans.end());
+
+    return ans;
+}
